2b17c2354e4ad7a5156d9c30984c36ae59af007e
German Valencia Vargas <gvalenncia@gmail.com>
2019-02-12 16:20:36 -0500

    Implementing a simple queries and starting a graphql server

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,10 +1,39 @@
-import myLocation, { getGreeting, message, name } from './myModule'
-import add, { substract } from './math'
+import { GraphQLServer } from 'graphql-yoga'
 
-console.log( message )
-console.log( name )
-console.log( myLocation )
-console.log( getGreeting('tecla') )
+// Type definitions (Schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
 
-console.log(add(1, 1))
-console.log(substract(1, 1))
+// Resolvers - functions that will be executed for every action or query happening in the api
+const  resolvers = {
+    Query: {
+        hello() {
+            return 'hello, this is my first query!'
+        },
+        name() {
+            return 'German'
+        },
+        location(){
+            return 'Bogota'
+        },
+        bio(){
+            return 'Superman'
+        }
+    }
+}
+
+// Startin gthe serer
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('up and running!')
+})
--- graphql-basics/src/math.js
+++
@@ -1,9 +0,0 @@
-const add = (oper1, oper2) => {
-    return oper1 + oper2
-}
-
-const substract = (oper1, oper2) => {
-    return oper1 - oper2
-}
-
-export { add as default, substract }
