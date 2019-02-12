acb6ea2bbd1dcd5615f89092179b324479203e99
German Valencia Vargas <gvalenncia@gmail.com>
2019-02-12 15:50:19 -0500

    Setting up babel and importing simple variables

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,5 @@
+import myLocation, { message, name } from './myModule'
+
+console.log( message )
+console.log( name )
+console.log( myLocation )
---
+++ graphql-basics/src/myModule.js
@@ -0,0 +1,6 @@
+const message = 'from my module'
+const name = 'German'
+
+const location = 'Bogota'
+
+export { message, name, location as default }
