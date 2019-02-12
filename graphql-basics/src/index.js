ec8fc139b20e3c874c19bd53ca81100cc05f3e1f
German Valencia Vargas <gvalenncia@gmail.com>
2019-02-12 15:58:50 -0500

    simple challenge importing a pair of fucntions

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,10 @@
 import myLocation, { getGreeting, message, name } from './myModule'
+import add, { substract } from './math'
 
 console.log( message )
 console.log( name )
 console.log( myLocation )
 console.log( getGreeting('tecla') )
+
+console.log(add(1, 1))
+console.log(substract(1, 1))
---
+++ graphql-basics/src/math.js
@@ -0,0 +1,9 @@
+const add = (oper1, oper2) => {
+    return oper1 + oper2
+}
+
+const substract = (oper1, oper2) => {
+    return oper1 - oper2
+}
+
+export { add as default, substract }
