2b8537b841c45cfa8683d58124e26c4eac70a273
German Valencia Vargas <gvalenncia@gmail.com>
2019-02-13 09:07:44 -0500

    Creting a users and posts queries and implementing a simple filter

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,13 +1,26 @@
 import { GraphQLServer } from 'graphql-yoga'
-import { userInfo } from 'os';
 
 // Scalar types in graphql: String, Boolean, Int, float, ID
+
+// User mock data
+const users = [
+    {id: '1', name: 'german', email: 'gvalencia@gmail.com'},
+    {id: '2', name: 'andrew', email: 'andrew@example.com'},
+    {id: '3', name: 'sara', email: 'sara@example.com'}
+]
+
+// Post mock data
+const posts = [
+    {id: '10', title: 'My first post', body: 'this is my first post', author: '1'},
+    {id: '20', title: 'just another post', body: 'this is just another post body mate', author: '1'},
+    {id: '30', title: 'have fun', body: 'remember to have fun while coding', author: '2'}
+]
 
 // Type definitions (Schema)
 const typeDefs = `
     type Query {
-        add(a: Float!, b: Float!): Float!
-        greeting(name: String): String!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
@@ -23,20 +36,28 @@ const typeDefs = `
         id: ID!
         title: String!
         body: String!
+        user: User!
     }
 `
 
 // Resolvers - functions that will be executed for every action or query happening in the api
 const  resolvers = {
     Query: {
-        add(parent, args, ctx, info){
-            return args.a + args.b
-        },
-        greeting( parent, args, ctx, info ){
-            if(args.name) {
-                return `Hello, ${args.name}`
+        users(parent, args, ctx, info){
+            if(args.query){
+                return users.filter((user) => {
+                    return user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+                })
             } else {
-                return 'Hello everyone'
+                return users
+            }
+        },
+        posts(parent, args, ctx, info){
+            if(args.query){
+                return posts.filter((post) => {
+                    return post.title.toLocaleLowerCase().includes(args.query.toLocaleLowerCase()) 
+                    || post.body.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+                })
             }
         },
         me(){
@@ -56,7 +77,7 @@ const  resolvers = {
     }
 }
 
-// Startin gthe serer
+// Starting the server
 const server = new GraphQLServer({
     typeDefs,
     resolvers
